082be09a1a9b4a510fec2f31ed8a11034dfa600f
Daphne <daphneipp@gmail.com>
2017-01-29 21:36:54 -0500

    Code for learning xor is working, but tensorboard results are not

--- XOR/xor_network.py
+++ XOR/xor_network.py
@@ -3,13 +3,24 @@ import tensorflow as tf
 import random
 import time
 
-def get_batch():
-	options = [([0,1], [1, 0]), ([0,0], [0, 1]), ([1, 1], [0, 1]), ([1,0], [1, 0])]
-	chosen = random.choice(options)
+def get_batch(batch_size):
+	options = [([0,1], [1, 0]), ([0,0], [1, 0]), ([1, 1], [0, 1]), ([1,0], [1, 0])]
+	# options = [([0,1], [1, 0]), ([0,0], [1, 0]), ([1, 1], [0, 1])]#, ([1,0], [1, 0])]
 
-	x = np.array(chosen[0], dtype='float').reshape([1,2])
-	y = np.array(chosen[1], dtype='float').reshape([1,2])
+	x = np.zeros([batch_size, 2], dtype='float')
+	y = np.zeros([batch_size, 2], dtype='float')
+
+	for i in xrange(0, batch_size):
+		chosen = random.choice(options)
+		x[i,:] = np.array(chosen[0], dtype='float').reshape([1,2])
+		y[i,:] = np.array(chosen[1], dtype='float').reshape([1,2])
 	return (x, y) 
+
+	# for i in xrange(0, batch_size):
+		# chosen = options[i]
+		# x[i,:] = np.array(chosen[0], dtype='float').reshape([1,2])
+		# y[i,:] = np.array(chosen[1], dtype='float').reshape([1,2])
+	# return (x, y) 
 
 def variable_summaries(variables):
 	"""Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
@@ -25,10 +36,12 @@ def variable_summaries(variables):
 			tf.summary.histogram('histogram', var)
 	 
 if __name__ == '__main__':
+	batch_size = 100
+
 	with tf.Graph().as_default():
 		# Define variables.
-		x = tf.placeholder(tf.float32, [1, 2], name='x')
-		y = tf.placeholder(tf.float32, [1, 2], name='y') 
+		x = tf.placeholder(tf.float32, [batch_size, 2], name='x')
+		y = tf.placeholder(tf.float32, [batch_size, 2], name='y') 
 
 		with tf.name_scope('hidden0'):
 			weights0 = tf.Variable(tf.truncated_normal([2, 10], mean=0, stddev=0.01), name='weights');
@@ -42,7 +55,7 @@ if __name__ == '__main__':
 			weights2 = tf.Variable(tf.truncated_normal([10, 2], mean=0, stddev=0.01), name='weights');
 			biases2 = tf.Variable(tf.zeros([2]), name='biases')
 
-		# variable_summaries([weights0, biases0, weights1, biases1, weights2, biases2])
+		variable_summaries([weights0, biases0, weights1, biases1, weights2, biases2])
 
 		#hidden1 = tf.nn.relu(tf.matmul(x, tf.get_variable('hidden1/weights')) + tf.get_variable('hidden1/biases'))
 		#hidden2 = tf.nn.relu(tf.matmul(hidden1, tf.get_variable('hidden2/weights')) + tf.get_variable('hidden2/biases'))
@@ -52,10 +65,9 @@ if __name__ == '__main__':
 
 		loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(output, y), name='loss')
 
-		# Generates a snapshot of the loss function every time a summary is written out
 		tf.scalar_summary('loss', loss)
 
-		learning_rate = 0.01
+		learning_rate = 0.1
 
 		# optimizer = tf.train.AdamOptimizer(learning_rate)
 		optimizer = tf.train.GradientDescentOptimizer(learning_rate)
@@ -71,8 +83,8 @@ if __name__ == '__main__':
 			init = tf.global_variables_initializer()
 			sess.run(init)	
 
-			for step in xrange(1, 3000):
-				data_x, data_y = get_batch()
+			for step in xrange(1, 5000):
+				data_x, data_y = get_batch(batch_size)
 				feed_dict = {x: data_x, y: data_y}
 
 				start_time = time.time()
@@ -84,15 +96,17 @@ if __name__ == '__main__':
 					summary_writer.add_summary(summary_str, step)
 
 				if step % 100 == 0:
-					print data_x
-					print 'Step %d: loss = %.2f (%.3f sec)' % (step, loss_value, duration)	
+					# print data_x
+					num_correct_op = tf.equal(tf.argmax(data_y, 1), tf.argmax(output, 1))
+					accuracy = np.sum(sess.run(num_correct_op, feed_dict=feed_dict)) / float(batch_size)
+					print 'Step %d: loss = %.2f, batch acc = %.2f (%.3f sec)' % (step, loss_value, accuracy, duration)	
 
 			# check the results on some examples
-			for i in xrange(0,10):
-				data = get_batch()
-				f_dict = {x: data[0], y: data[1]}	
-				print f_dict
-				out = sess.run(output, f_dict)
-				print out[0, 0] > out[0, 1]
-				print out
-				print "\n"
+			# for i in xrange(0,10):
+				# data = get_batch()
+				# f_dict = {x: data[0], y: data[1]}	
+				# print f_dict
+				# out = sess.run(output, f_dict)
+				# print out[0, 0] > out[0, 1]
+				# print out
+				# print "\n"
