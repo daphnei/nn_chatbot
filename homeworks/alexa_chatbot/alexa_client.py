4965b780ef4fe53bb4033d29506a97db9c0a36d4
Krzysztof Jordan <krzysztofjordan@Krzysztofs-MacBook-Pro.local>
2017-02-14 17:48:47 -0500

    added assurances for having new lines in every message at the end, fixed server not resetting utterance and just concatenating them, added network configuration files

--- homeworks/alexa_chatbot/alexa_client.py
+++ homeworks/alexa_chatbot/alexa_client.py
@@ -1,22 +1,30 @@
 import socket
-import sys
+import json
 
 
 def talk_to_server(user_utterance):
 	reply = ""
 
+	with open('config.json') as config_file:
+		data = json.load(config_file)
+		serv_addr = data["server_address"]
+		port = data["port"]
+
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_address = ('lvn403-6.grasp.upenn.edu', 62219)
+	server_address = (serv_addr, port)
 	sock.connect(server_address)
 
 	try:
-		sock.sendall(user_utterance)
+		sock.sendall(user_utterance + '\n')
 
-		amount_expected = 150
 		print("HERE")
 
-		while "\n" not in reply and len(reply) <= amount_expected:
+		while "\n" not in reply:
 			data = sock.recv(16)
+			if len(data) <= 0:
+				print("something went wrong")
+				break
+
 			print("here with " + str(data))
 
 			reply += data
