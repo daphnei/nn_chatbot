f1ebcc3161dbbf97af087aa6c9ac6d60325a9df7
Ubuntu <ubuntu@ip-172-31-66-128.ec2.internal>
2017-04-29 22:54:47 +0000

    train script for seq2seq

---
+++ seq2seq/scripts/seq2seq_bookcorpus/run.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+DATA_PATH="/data"
+MODEL_PATH="/data/models/books_v1"
+PREDICTIONS_PATH="$MODEL_PATH/predictions"
+
+SEQ2SEQ_PATH="../.."
+MODEL_YAML="./model.yaml"
+
+export PYTHONPATH=$SEQ2SEQ_PATH
+
+VOCAB_SOURCE=${DATA_PATH}/vocab.tok.txt
+VOCAB_TARGET=${DATA_PATH}/vocab.tok.txt
+
+TRAIN_SOURCES=${DATA_PATH}/input.txt
+TRAIN_TARGETS=${DATA_PATH}/output.txt
+
+TRAIN_STEPS=1000000
+
+mkdir -p $MODEL_PATH
+
+op=$1
+
+
+if [ "$op" = "train" ]; then
+     # $SEQ2SEQ_PATH/example_configs/train_seq2seq.yml,
+     # $SEQ2SEQ_PATH/example_configs/text_metrics_bpe.yml" \
+  python "$SEQ2SEQ_PATH/bin/train.py"  \
+    --config_paths="$MODEL_YAML" \
+    --model_params "
+        vocab_source: $VOCAB_SOURCE
+        vocab_target: $VOCAB_TARGET" \
+    --input_pipeline_train "
+      class: ParallelTextInputPipeline
+      params:
+        source_files:
+          - $TRAIN_SOURCES
+        target_files:
+          - $TRAIN_TARGETS" \
+    --input_pipeline_dev "
+      class: ParallelTextInputPipeline
+      params:
+         source_files:
+          - $TRAIN_SOURCES
+         target_files:
+          - $TRAIN_TARGETS" \
+    --batch_size 32 \
+    --train_steps $TRAIN_STEPS \
+    --output_dir $MODEL_PATH \
+    --gpu_allow_growth \
+    --gpu_memory_fraction 0.8 \
+    --buckets 20,40
+
+elif [ "$op" = "test" ]; then
+python -m bin.infer \
+  --tasks "
+    - class: DecodeText
+    - class: DumpBeams
+      params:
+        file: ${PREDICTIONS_PATH}/beams.npz" \
+  --model_dir $MODEL_PATH \
+  --model_params "
+    inference.beam_search.beam_width: 5" \
+  --input_pipeline "
+    class: ParallelTextInputPipeline
+    params:
+      source_files:
+        - $DEV_SOURCES" \
+  > ${PREDICTIONS_PATH}/predictions.txt
+#else
+#  echo "Operation not supported."
+fi
