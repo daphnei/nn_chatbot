3f13f41a2abcacfc24477523c2b1fb82908f5c5c
Daphne <daphneipp@gmail.com>
2017-05-02 13:52:44 +0000

    running parameters

--- seq2seq/scripts/seq2seq_bookcorpus/run.sh
+++ seq2seq/scripts/seq2seq_bookcorpus/run.sh
@@ -12,8 +12,11 @@ export PYTHONPATH=$SEQ2SEQ_PATH
 VOCAB_SOURCE=${DATA_PATH}/vocab.tok.txt
 VOCAB_TARGET=${DATA_PATH}/vocab.tok.txt
 
-TRAIN_SOURCES=${DATA_PATH}/input.txt
-TRAIN_TARGETS=${DATA_PATH}/output.txt
+TRAIN_SOURCES=${DATA_PATH}/train_input.txt
+TRAIN_TARGETS=${DATA_PATH}/train_output.txt
+
+DEV_SOURCES=${DATA_PATH}/dev_input.txt
+DEV_TARGETS=${DATA_PATH}/dev_output.txt
 
 TRAIN_STEPS=1000000
 
@@ -22,11 +25,13 @@ mkdir -p $MODEL_PATH
 op=$1
 
 
+    #--config_paths="$CONFIG_PATH/text_metrics.yml, $CONFIG_PATH/model.yaml, " \
+
 if [ "$op" = "train" ]; then
      # $SEQ2SEQ_PATH/example_configs/train_seq2seq.yml,
      # $SEQ2SEQ_PATH/example_configs/text_metrics_bpe.yml" \
   python "$SEQ2SEQ_PATH/bin/train.py"  \
-    --config_paths="$CONFIG_PATH/text_metrics.yml, $CONFIG_PATH/model.yaml, " \
+    --config_paths="$CONFIG_PATH/model.yaml" \
     --model_params "
         vocab_source: $VOCAB_SOURCE
         vocab_target: $VOCAB_TARGET" \
@@ -41,9 +46,9 @@ if [ "$op" = "train" ]; then
       class: ParallelTextInputPipeline
       params:
          source_files:
-          - $TRAIN_SOURCES
+          - $DEV_SOURCES
          target_files:
-          - $TRAIN_TARGETS" \
+          - $DEV_TARGETS" \
     --batch_size 32 \
     --train_steps $TRAIN_STEPS \
     --output_dir $MODEL_PATH \
