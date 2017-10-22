c5ad46aec1de3feb896c64db80cb4b2dd1bd10a9
Daphne <daphneipp@gmail.com>
2017-10-22 12:35:11 -0400

    file forgot to commit

--- remove_proper_names/word_swaps.py
+++ remove_proper_names/word_swaps.py
@@ -23,6 +23,7 @@ def is_date(string):
 def is_number(s):
  	try:
 		v = float(s)
+    # Crappy hack to capture most year dates and return False for them.
  		return v <= 1700 or  v >= 3000
  	except: # (ValueError, OverflowError):
  		pass
@@ -45,19 +46,17 @@ if __name__ == "__main__":
        and sentence tags in between them
     '''
 
-	in_file = "/home/ubuntu/nn_chatbot/seq2seq/scripts/data/books_large_p1.txt" 
-	out_file = "/home/ubuntu/nn_chatbot/seq2seq/scripts/data/books_large_p1_simplified.txt" 
-	#in_file = "/media/snake/daphne/school/nn_chatbot/big_book_corpus/in_sentences/books_large_p1.txt"
-	#out_file = "/media/snake/daphne/school/nn_chatbot/big_book_corpus/in_sentences/books_large_p1_simplified.txt"
+	# in_file = "/home/ubuntu/nn_chatbot/seq2seq/scripts/data/books_large_p1.txt" 
+	# out_file = "/home/ubuntu/nn_chatbot/seq2seq/scripts/data/books_large_p1_simplified.txt" 
+	in_file = "/media/snake/daphne/school/nn_chatbot/big_book_corpus/in_sentences/books_large_p1.txt"
+	out_file = "/media/snake/daphne/school/nn_chatbot/big_book_corpus/in_sentences/books_large_p1_simplified_2.txt"
 
 	# Simplify the text to only have these many unique names
 	NUM_NAMES = 100
 
 	output = ""
 
-	word_tokenizer = nltk.TreebankWordTokenizer()
-
-	proper_names_trie = proper_names.load()
+	proper_names_trie = proper_names.get_or_create_proper_names()
 
 	names_so_far = {}
 	dates = set()
@@ -76,19 +75,19 @@ if __name__ == "__main__":
 					for i in xrange(0, len(words)):
 						word = words[i]
 						if word.lower() in proper_names_trie: #and word[0] == word[0].upper():
-							# This is a proper name. Check if it is one we have seen before in this text
+							# This is a proper name. Replace it with _name_%HASH(name)%
 							replacement_name = "_name_" + str(zlib.adler32(word) % NUM_NAMES)
 							words[i] = replacement_name
 						elif is_number(word):
 							words[i] = "_number_"
-							#if word not in numbers:
-							#	print("New number: " + word)
-							#	numbers.add(word)
+              # Keep track of all 'number' words, in case we're interested in look at this.
+							if word not in numbers:
+								numbers.add(word)
 						elif is_date(word):
 							words[i] = "_date_"
-							#if word not in dates:
-							#	print("New date: " + word)
-							#	dates.add(word)
+              # Keep track of all 'number' w, in case we're interested in look at this.
+							if word not in dates:
+								dates.add(word)
 		
 					out_line = " ".join(words)
 					f_out.write(out_line)
