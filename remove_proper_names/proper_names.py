703083249d4fd45bb09b2e9410931ae09fa7575c
Daphne <daphneipp@gmail.com>
2017-10-22 12:08:36 -0400

    cleaned up comments in code to swap out proper names

--- remove_proper_names/proper_names.py
+++ remove_proper_names/proper_names.py
@@ -1,26 +1,18 @@
 import urllib2
 import csv
-import pdb
 from pygtrie import Trie
 from pygtrie import PrefixSet
 import pickle
 import os
 
-def load():
-	if os.path.exists('proper_names.pickle'):
-		with open('proper_names.pickle', 'r') as file:
-			return pickle.load(file)
-	else:
-	 	return create_trie()
-
-def create_trie():
+def _create_trie():
 	tsvs = ["https://www2.census.gov/topics/genealogy/1990surnames/dist.female.first",
 			"https://www2.census.gov/topics/genealogy/1990surnames/dist.male.first"]
 			# "https://www2.census.gov/topics/genealogy/1990surnames/dist.all.last"]
 
-	# A harded-coded list of exceptions. (names that are more often see as common noun
+	# A harded-coded list of exceptions. (names that are more often seen as common noun
 	# at the front of sentences.)
-	exceptions = ["winter", "grant", "van", "son", "young", "royal", "long", "june", "august", "joy", "young", "aura", "ray", "ok", "harmony", "ha", "sun", "in", "many", "see", "so", "my", "may", "an", "les", "will", "love", "man"]
+	exceptions = ["winter", "grant", "van", "son", "young", "royal", "long", "june", "august", "joy", "young", "aura", "ray", "ok", "harmony", "ha", "sun", "in", "many", "see", "so", "my", "may", "an", "les", "will", "love", "man", "major", "faith"]
 
 	names = []
 	for tsv_url in tsvs:
@@ -42,9 +34,16 @@ def create_trie():
 
 	return trie
 
+def get_or_create_proper_names():
+	if os.path.exists('proper_names.pickle'):
+		with open('proper_names.pickle', 'r') as file:
+			return pickle.load(file)
+	else:
+	 	return _create_trie()
+
 
 if __name__ == "__main__":
-	p = create_trie()
+	p = _create_trie()
 	print(p.__contains__("daphne"))
 	print(p.__contains__("xavier"))
 	print(p.__contains__("sally"))
